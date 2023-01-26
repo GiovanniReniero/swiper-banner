881704d23ec1978576d668c7aa3390a46427b402
GiovanniReniero <giovanni.reniero@gmail.com>
2023-01-26 15:34:11 +0000

    merge javascript files into one

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: "auto",
   spaceBetween: 10,
 });
+
+var mainNavSwiper = document.querySelector(".swiper").swiper;
+
+var resizeTimeOut = null;
+window.addEventListener("resize", function () {
+  clearTimeout(resizeTimeOut);
+  resizeTimeOut = setTimeout(() => {
+    var w = window.innerWidth;
+    console.log(`${w} pixels in width!`);
+    swiper.slideTo(0);
+  }, 1000);
+});
+
+if (w > 1201) {
+  mainNavSwiper.disable();
+  console.log(`${w} pixels in width, Swiper is disabled`);
+} else {
+  mainNavSwiper.enable();
+  console.log(`${w} pixels in width, Swiper is enabled`);
+}
